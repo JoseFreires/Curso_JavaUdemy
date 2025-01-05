ed35d32c8359e7a6097b89c4a2f2eeab15437f20
Jose Freires <joseguifreires@gmail.com>
2025-01-05 19:50:25 -0300

    New folder: Generics, Set and Map

--- curso_java_GenericsSetMap/aula03/src/application/Program.java
+++ curso_java_GenericsSetMap/aula03/src/application/Program.java
@@ -1,10 +1,27 @@
+/* 
+Tema: Introdução ao Curinga
+O supertipo de qualquer tipo de lista é List<?>. Este é um tipo curinga.
+
+Com tipos curinga podemos fazer métodos que recebem um genérico de "qualquer tipo";
+Porém não é possível adicionar dados a uma coleção de tipo curinga, porque o compilador não
+sabe qual é o tipo específico que foi instanciada.
+
+ */
+
 package application;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Program {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
 	}
 
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+	}
 }
